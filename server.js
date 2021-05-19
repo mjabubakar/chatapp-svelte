9ff6d673ac6230e18ffdf08ea2c71530133b083b
Muhammad Jamil Abubakar <imuhdjml@gmail.com>
2021-05-19 14:27:57 +0100

    app is server side rendered

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv/config");
+
+const PORT = process.env.PORT || 5000;
+const TEMPLATE = "dist/__app.html";
+
+// serve assets, if they exist
+app.use(express.static("dist"));
+
+// otherwise serve Routify
+app.get("*", async (req, res) => {
+  res.sendFile(TEMPLATE, { root: __dirname });
+});
+
+// start server
+app.listen(PORT);
+console.log("serving on port", PORT);
